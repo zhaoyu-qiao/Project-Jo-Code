ffbf95e55618822405ded6b1824741603a68879e
Zhaoyu Qiao <qiao.zy7@gmail.com>
2019-11-02 11:39:55 -0400

    Add different functions for the different word types

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -13,9 +13,16 @@ let verbUrl = baseUrl + verbString + "&random=true";
 let adjectiveUrl = baseUrl + adjectiveString + "&random=true";
 let adverbUrl = baseUrl + adverbString + "&random=true";
 
-let nounArray = [];
-
 console.log(nounUrl);
+console.log(verbUrl);
+console.log(adjectiveUrl);
+console.log(adverbUrl);
+
+let nounArray = [];
+let verbArray = [];
+let adjArray = [];
+let advArray = [];
+
 
 
 function getNouns() {
@@ -38,19 +45,71 @@ function getNouns() {
     }
 }
 
-// function writeNouns() {
-//     for (let i = 0; i < nounArray; i++) {
-//         $("#noun"").append(nounArray[i]);
-//         console.log(nounArray);
-//     }
-// }
+function getVerbs() {
 
+    for (let i = 0; i < 5; i++) {
+        $.ajax({
+            method: "POST",
+            beforeSend: function (request) {
+                request.setRequestHeader("X-Mashape-Key", "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea");
+            },
+            method: "GET",
+            url: verbUrl,
+        }).then(function (response) {
+            console.log(response);
+            console.log(response.word);
+
+            $("#verb").append('<option class="verb">' + response.word + '</option>');
+
+        })
+    }
+}
+
+function getAdjs() {
+
+    for (let i = 0; i < 5; i++) {
+        $.ajax({
+            method: "POST",
+            beforeSend: function (request) {
+                request.setRequestHeader("X-Mashape-Key", "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea");
+            },
+            method: "GET",
+            url: adjectiveUrl,
+        }).then(function (response) {
+            console.log(response);
+            console.log(response.word);
+
+            $("#adjective").append('<option class="adjective">' + response.word + '</option>');
+
+        })
+    }
+}
+
+function getAdvs() {
+
+    for (let i = 0; i < 5; i++) {
+        $.ajax({
+            method: "POST",
+            beforeSend: function (request) {
+                request.setRequestHeader("X-Mashape-Key", "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea");
+            },
+            method: "GET",
+            url: adverbUrl,
+        }).then(function (response) {
+            console.log(response);
+            console.log(response.word);
+
+            $("#adverb").append('<option class="adverb">' + response.word + '</option>');
+
+        })
+    }
+}
 $(document).ready(function () {
     // Get the words
     getNouns();
-    //getVerbs();
-    //getAdjs();
-    //getAdvs();
+    getVerbs();
+    getAdjs();
+    getAdvs();
     //getLocations();
     //...
     //writeNouns();
