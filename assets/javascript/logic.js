8cdf2f6088169a65b61281e6542339125c764120
Zhaoyu Qiao <qiao.zy7@gmail.com>
2019-11-02 11:14:08 -0400

    able to pull the nouns from wordsapi, and then save them in nounArray, then write them into HTML

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,11 +1,16 @@
+// This is the js for page 4, when user input their choices, and make selection
 // Get 5 nouns /words from words api
 // curl 'https://wordsapiv1.p.mashape.com/words/?partOfSpeech=adverb&random=true' -H "X-Mashape-Key: c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea"
 let baseUrl = "https://wordsapiv1.p.mashape.com/words/";
 let nounString = "?partOfSpeech=noun";
 let nounUrl = baseUrl + nounString + "&random=true";
+let nounArray = [];
+
 console.log(nounUrl);
 
+
 function getNouns() {
+
     for (let i = 0; i < 5; i++) {
         $.ajax({
             method: "POST",
@@ -16,11 +21,39 @@ function getNouns() {
             url: nounUrl,
         }).then(function (response) {
             console.log(response);
+            console.log(response.word);
+
+            $("#noun").append('<option class="noun" id="noun1">' + response.word + '</option>');
+
         })
     }
 }
 
+// function writeNouns() {
+//     for (let i = 0; i < nounArray; i++) {
+//         $("#noun"").append(nounArray[i]);
+//         console.log(nounArray);
+//     }
+// }
 
-$(window).on("load", function () {
-    $("#dropdownMenuButton").on("click", getNouns)
-});
+$(document).ready(function () {
+    // Get the words
+    getNouns();
+    //getVerbs();
+    //getAdjs();
+    //getAdvs();
+    //getLocations();
+    //...
+    //writeNouns();
+    // Put the words into html form to the correct input.
+
+    // User select the words from the drop-downs
+
+    // Submit
+})
+
+
+
+// $(window).on("load", function () {
+
+// });
