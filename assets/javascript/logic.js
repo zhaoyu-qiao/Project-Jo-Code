56a2e6cbf91a45b279c9ac9ae4ecff2b5093165c
Zhaoyu Qiao <qiao.zy7@gmail.com>
2019-11-02 00:01:47 -0400

    Added drop down button using bootstrap, added js function to grab five random nouns from words api, for loop is in use, there may be easier ways

---
+++ assets/javascript/logic.js
@@ -0,0 +1,26 @@
+// Get 5 nouns /words from words api
+// curl 'https://wordsapiv1.p.mashape.com/words/?partOfSpeech=adverb&random=true' -H "X-Mashape-Key: c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea"
+let baseUrl = "https://wordsapiv1.p.mashape.com/words/";
+let nounString = "?partOfSpeech=noun";
+let nounUrl = baseUrl + nounString + "&random=true";
+console.log(nounUrl);
+
+function getNouns() {
+    for (let i = 0; i < 6; i++) {
+        $.ajax({
+            method: "POST",
+            beforeSend: function (request) {
+                request.setRequestHeader("X-Mashape-Key", "c689d162f5mshf499a4cc1699b78p184059jsn3776ce81a3ea");
+            },
+            method: "GET",
+            url: nounUrl,
+        }).then(function (response) {
+            console.log(response);
+        })
+    }
+}
+
+
+$(window).on("load", function () {
+    $("#dropdownMenuButton").on("click", getNouns)
+});
